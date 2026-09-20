36c79a124bc9c2383a6377ee77467980ec61e683
Ростислав Дурягін <rostyslav.duriahin@lnu.edu.ua>
2026-09-20 14:25:57 +0200

    Fix flake8 and add library for jwt token

--- config/settings.py
+++ config/settings.py
@@ -82,6 +82,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
