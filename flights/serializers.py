9bc7db94b4a24bc59296d1f54f610668fe524079
Rosyk <115212316+Rostyk-D@users.noreply.github.com>
2026-09-23 19:28:25 +0200

    Validate airplane baggage weight limits

--- flights/serializers.py
+++ flights/serializers.py
@@ -30,8 +30,32 @@ class AirplaneSerializer(serializers.ModelSerializer):
             "name",
             "rows",
             "seats_in_row",
+            "min_baggage_weight",
+            "max_baggage_weight",
             "airplane_type",
         )
+
+    def validate(self, attrs):
+        min_weight = attrs.get(
+            "min_baggage_weight",
+            getattr(self.instance, "min_baggage_weight", 2),
+        )
+        max_weight = attrs.get(
+            "max_baggage_weight",
+            getattr(self.instance, "max_baggage_weight", 10),
+        )
+
+        if min_weight > max_weight:
+            raise serializers.ValidationError(
+                {
+                    "max_baggage_weight": (
+                        "Maximum baggage weight cannot be less "
+                        "than minimum baggage weight."
+                    )
+                }
+            )
+
+        return attrs
 
 
 class FlightSerializer(serializers.ModelSerializer):
